7789fdf3eadc46a0a280427a20c2faeba8c363a8
Vignesh M <Vignesh.M14@ibm.com>
2025-01-25 12:19:13 +0530

    App import modified

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.scss'
 import './index.css'
-import App from './App.tsx'
+import { App } from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
