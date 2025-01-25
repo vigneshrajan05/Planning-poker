95d6ab28f4fe7e3a108cd71605d2c0b92f2353ef
SOWBARNIGAA SHANMUGAVADIVEL <85994406+Sowbarnigaa@users.noreply.github.com>
2025-01-25 12:58:08 +0530

    Update HomeComponent.tsx

--- ui/src/Home/HomeComponent.tsx
+++ ui/src/Home/HomeComponent.tsx
@@ -1,5 +1,11 @@
 export const HomeComponent = () => {
   return (
+    <>
     <div style={{paddingTop: '48px', paddingLeft: 0}}>HomeComponent</div>
+       <div style={{ alignItems: 'flex-end' }} >
+        <PlanComponent />
+      </div >
+    </>
+      
   )
 }
